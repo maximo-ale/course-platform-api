2a937c8dc431a6a451458b24858bf099e6fe4ec6
maximo-ale <maximoale20000@gmail.com>
2025-08-14 18:34:45 -0300

    Improved auth verification

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
     const token = req.header('Authorization')?.split(" ")[1];
     if (!token) {
         return res.status(401).json({message: 'No token'});
@@ -10,6 +10,13 @@ const auth = (req, res, next) => {
         req.user = decoded;
         req.userId = decoded.userId;
         req.userCourses = decoded.userCourses;
+
+        // Only users from the DB
+        const userInDB = await User.findById(decoded.userId);
+        if (!userInDB){
+            return res.status(403).json({message: 'Invalid token'});
+        }
+        
         next();
     } catch {
         return res.status(403).json({message: 'Invalid token'});
